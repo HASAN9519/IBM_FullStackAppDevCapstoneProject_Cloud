a6f9d57d0083069656ca95c8dee8ffce0d6869d3
HASAN9519 <shehab10111995@gmail.com>
2025-06-18 02:31:18 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -113,7 +113,9 @@ def get_dealer_details(request, dealer_id):
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
     if(dealer_id):
-        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        # endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        endpoint = "/fetchReviews/dealer/id/"+str(dealer_id)
+
         reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
